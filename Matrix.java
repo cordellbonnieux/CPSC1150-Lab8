ee072181636ccd659cb8f0cc7d5f49a66355cf0b
Cordell <cordellbonnieux@gmail.com>
2021-07-07 13:34:12 -0700

    Created genMatrix method and skeleton of readM().

--- Matrix.java
+++ Matrix.java
@@ -1,7 +1,27 @@
+import java.util.*;
 public class Matrix {
     public static void main(String[] args) {
 
 
-        
+
+    }
+    public static int readM(){
+
+        //
+    }
+
+    public static double[][] genMatrix(int m) {
+
+        // create double matrix of size m x m
+        double[][] matrix = new double[m][m];
+
+        // fill the matrix with random values
+        for (int row = 0; row < m; row++) {
+            for (int column = 0; column < m; column++) {
+                matrix[row][column] = 0 + Math.random() * 99.99;
+            }
+        }
+
+        return matrix;
     }
 }
