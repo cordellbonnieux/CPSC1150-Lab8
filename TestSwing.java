6f9c15c37a8f3ab4ce29f1b239025ed03c8798e1
Cordell <cordellbonnieux@gmail.com>
2021-07-07 10:32:00 -0700

    first commit

---
+++ TestSwing.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+public class TestSwing {
+	public static void main(String[] args){
+
+		String userInput = JOptionPane.showInputDialog(null, "Enter a positive number : ");
+		int num = Integer.parseInt(userInput);
+		if (num > 0)
+			JOptionPane.showMessageDialog(null, num*num + " is the square of " + num );
+		else 
+			JOptionPane.showMessageDialog(null, "Invalid Data: " + num + " is not a positive number!"  );
+	}
+
+	public static int average(int[] array) {
+
+
+	}
+
+	public static double average(double[] array) {
+
+		
+	}
+}
