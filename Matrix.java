ee9308a45d7ed6528446093e7919582c97c609a1
Cordell <cordellbonnieux@gmail.com>
2021-07-07 16:39:46 -0700

    Q3 complete, documentation next

--- Matrix.java
+++ Matrix.java
@@ -5,11 +5,16 @@ public class Matrix {
         // read a matrix size
         int m = readM();
 
-        // generate a matrix 
+        // Q1 - generate a matrix 
         double[][] matrix = genMatrix(m);
 
-        //test print
+        // Q2 - print matrix
         printMatrix(matrix);
+
+        // Q3 - is matrix symmetric?
+        // this is a symmetric array you can uncomment to test the method
+        //double[][] testArr = {{1,2,3},{2,4,5},{3,5,8}};
+        System.out.println("Is this matrix symmetric? " + ((isSymmetric(matrix)) ? "Yes" : "No"));
 
 
     }
@@ -39,46 +44,7 @@ public class Matrix {
     }
 
     /**
-     * Prints a 2D matrix array of type double to the console
-     * @param matrix a 2D double array to be printed
-     */
-    public static void printMatrix(double[][] matrix) {
-
-        for (int row = 0; row < matrix.length; row++) {
-
-            // add a row heading
-            System.out.print("row #" + ((row < 9) ? "0" + (row+1) : (row+1)) + ":");
-            
-            for (int column = 0; column < matrix[row].length; column++) {
-
-                // truncate the double to 2 decimal points
-                double num = Math.floor(matrix[row][column] * 100) / 100;
-                String numS = String.valueOf(num);
-
-                // add a 0 at the begining if needed 
-                if (num < 10) {
-
-                    System.out.print("  0" + num);
-
-                    // add a 0 at the end if needed
-                    if (numS.length() < 4)
-                        System.out.print("0");
-
-                } else {
-
-                    System.out.print("  " + num);
-
-                    // add 0 to the end if needed
-                    if (numS.length() < 5)
-                        System.out.print("0");
-                }
-                System.out.print("  ");
-            }
-            System.out.println();
-        }
-    }
-
-    /**
+     * Question 1
      * Creates a 2D matrix (double array) of m x m size, filled with randomly generated doubles.
      * @param m size of the matrix
      * @return matrix of m x m size
@@ -96,5 +62,65 @@ public class Matrix {
         }
 
         return matrix;
+    }
+
+    /**
+     * Question 2
+     * Prints a 2D matrix array of type double to the console
+     * 
+     * Must rework this one
+     * @param matrix a 2D double array to be printed
+     */
+    public static void printMatrix(double[][] matrix) {
+
+        for (int row = 0; row < matrix.length; row++) {
+
+            // add a row heading
+            System.out.print("row #" + ((row < 9) ? "0" + (row+1) : (row+1)) + ":");
+            
+            for (int column = 0; column < matrix[row].length; column++) {
+
+                // truncate the double to 2 decimal points
+                double num = Math.floor(matrix[row][column] * 100) / 100;
+
+                // add a 0 at the begining if needed 
+                if (num < 10) {
+
+                    System.out.print("  0" + num);
+
+                    // add a 0 at the end if needed
+                    if (String.valueOf(num).length() < 4)
+                        System.out.print("0");
+
+                } else {
+
+                    System.out.print("  " + num);
+
+                    // add 0 to the end if needed
+                    if (String.valueOf(num).length() < 5)
+                        System.out.print("0");
+                }
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+    /**
+     * Question 3
+     * 
+     * @param matrix
+     * @return
+     */
+    public static Boolean isSymmetric(double[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++){
+
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != matrix[j][i])
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
